93624b7f10d824dcd7e97255141efffbff2cb93e
danOne23 <danonemun@gmail.com>
2020-05-25 16:10:02 +0200

    Added reverse dictionary

--- encryptor/encryptor/Dictionaries.cs
+++ encryptor/encryptor/Dictionaries.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 using System.Collections.Generic;
 using System.Text;
 
@@ -6,43 +7,59 @@ namespace encryptor
 {
     class CryptDictionary
     {
-
-        public Dictionary<string, long> encryptionDict()
+        private IDictionary<string, long> dict = new Dictionary<string, long>()
         {
-            Dictionary<string, long> returnDict = new Dictionary<string, long>();
-
-            for (int i = 0; i < decryptedChars.Length; i++)
-            {
-                returnDict.Add(decryptedChars[i], encryptedChars[i]);
-            }
-
-            return returnDict;
-        }
-
-        public Dictionary<long, string> DecryptionDict()
-        {
-            Dictionary<long, string> returnDict = new Dictionary<long, string>();
-
-            for (int i = 0; i < decryptedChars.Length; i++)
-            {
-                returnDict.Add(encryptedChars[i], decryptedChars[i]);
-            }
-
-            return returnDict;
-        }
-
-        private string[] decryptedChars = new string[]
-        {
-            "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q",
-            "r", "s", "t", "u", "v", "w", "x", "y", "z", "0", "1", "2", "3", "4", "5", "6", "7",
-            "8", "9", " ", "!", "?", ".", ","
+            {"a", 1000000007},
+            {"b", 1000000009},
+            {"c", 1000000021},
+            {"d", 1000000033},
+            {"e", 1000000087},
+            {"f", 1000000093},
+            {"g", 1000000097},
+            {"h", 10000000537},
+            {"i", 1000000123},
+            {"j", 1000000181},
+            {"k", 1000000207},
+            {"l", 1000000223},
+            {"m", 1000000241},
+            {"n", 1000000271},
+            {"o", 1000000289},
+            {"p", 1000000297},
+            {"q", 1000000321},
+            {"r", 1000000349},
+            {"s", 1000000363},
+            {"t", 1000000403},
+            {"u", 1000000019},
+            {"v", 10000000679},
+            {"w", 1000000061},
+            {"x", 1000000069},
+            {"y", 1000000063},
+            {"z", 1000000103},
+            {"0", 832583},
+            {"1", 832591},
+            {"2", 832597},
+            {"3", 832607},
+            {"4", 832613},
+            {"5", 832621},
+            {"6", 832627},
+            {"7", 832631},
+            {"8", 832633},
+            {"9", 832639},
+            {" ", 832568003},
+            {"!", 832568039},
+            {"?", 832568059},
+            {".", 832568069},
+            {",", 832568071}
         };
 
-        private long[] encryptedChars = new long[] 
+        public IDictionary<string, long> encryptionDict()
         {
-            1000000007, 1000000009, 1000000021, 1000000033, 1000000087, 1000000093, 1000000097, 10000000537, 1000000123, 1000000181, 1000000207, 1000000223, 1000000241, 1000000271, 1000000289,
-            1000000297, 1000000321, 1000000349, 1000000363, 1000000403, 1000000019, 10000000679, 1000000061, 1000000069, 10000000631, 1000000103, 832583, 832591, 832597, 832607, 832613, 832621,
-            832627, 832631, 832633, 832639, 832568003, 832568039, 832568059,832568069, 832568071
-        };
+            return dict;
+        }
+
+        public IDictionary<long, string> DecryptionDict()
+        {
+            return dict.ToDictionary(x => x.Value, x => x.Key);
+        }
     }
 }
