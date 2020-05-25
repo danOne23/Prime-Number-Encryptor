4e56bc3cd4d879de728c91c952def6ab8683b878
danOne23 <danonemun@gmail.com>
2020-05-25 16:27:22 +0200

    Changed file structure

---
+++ Decryptor.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace encryptor
+{
+    class Decryptor
+    {
+        public string decryptedStr;
+        private long encryptionKey;
+        private string seperator, encryptedStr;
+
+        public Decryptor(string encryptedInput, long key, string encryptedStrSeperator)
+        {
+            encryptionKey = key;
+            seperator = encryptedStrSeperator;
+            encryptedStr = encryptedInput;
+
+            string[] decryptedChars = new string[seperatedChars().Length];
+            for (int i = 0; i < seperatedChars().Length; i++)
+            {
+                decryptedChars[i] = decryptChar(Int64.Parse(seperatedChars()[i]));
+            }
+
+            decryptedStr = string.Join("", decryptedChars);
+        }
+
+        string[] seperatedChars()
+        {
+            return encryptedStr.Split(seperator);
+        }
+
+        string decryptChar(long encryptedChar)
+        {
+            encryptedChar /= encryptionKey;
+            CryptDictionary dict = new CryptDictionary();
+            return dict.DecryptionDict()[encryptedChar];
+        }
+
+    }
+}
