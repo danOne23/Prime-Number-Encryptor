5e86c658ce7d416f3e5b6605f6b7b68c80e1b5ca
danOne23 <danonemun@gmail.com>
2020-06-05 20:00:44 +0200

    Designed UI

---
+++ Form1.Designer.cs
@@ -0,0 +1,123 @@
+﻿namespace encryptor
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.inputTxt = new System.Windows.Forms.TextBox();
+            this.inputKey = new System.Windows.Forms.TextBox();
+            this.inputSepearator = new System.Windows.Forms.TextBox();
+            this.encryptBtn = new System.Windows.Forms.Button();
+            this.decryptBtn = new System.Windows.Forms.Button();
+            this.outputTxt = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // inputTxt
+            // 
+            this.inputTxt.Location = new System.Drawing.Point(12, 12);
+            this.inputTxt.Multiline = true;
+            this.inputTxt.Name = "inputTxt";
+            this.inputTxt.Size = new System.Drawing.Size(776, 20);
+            this.inputTxt.TabIndex = 0;
+            this.inputTxt.Text = "String";
+            // 
+            // inputKey
+            // 
+            this.inputKey.Location = new System.Drawing.Point(13, 39);
+            this.inputKey.Name = "inputKey";
+            this.inputKey.Size = new System.Drawing.Size(616, 20);
+            this.inputKey.TabIndex = 1;
+            this.inputKey.Text = "Encryption Key (whole number)";
+            // 
+            // inputSepearator
+            // 
+            this.inputSepearator.Location = new System.Drawing.Point(635, 39);
+            this.inputSepearator.Name = "inputSepearator";
+            this.inputSepearator.Size = new System.Drawing.Size(153, 20);
+            this.inputSepearator.TabIndex = 2;
+            this.inputSepearator.Text = "Seperator (one character)";
+            // 
+            // encryptBtn
+            // 
+            this.encryptBtn.Location = new System.Drawing.Point(12, 65);
+            this.encryptBtn.Name = "encryptBtn";
+            this.encryptBtn.Size = new System.Drawing.Size(399, 23);
+            this.encryptBtn.TabIndex = 3;
+            this.encryptBtn.Text = "Encrypt";
+            this.encryptBtn.UseVisualStyleBackColor = true;
+            // 
+            // decryptBtn
+            // 
+            this.decryptBtn.Location = new System.Drawing.Point(417, 65);
+            this.decryptBtn.Name = "decryptBtn";
+            this.decryptBtn.Size = new System.Drawing.Size(370, 23);
+            this.decryptBtn.TabIndex = 4;
+            this.decryptBtn.Text = "Decrypt";
+            this.decryptBtn.UseVisualStyleBackColor = true;
+            // 
+            // outputTxt
+            // 
+            this.outputTxt.Location = new System.Drawing.Point(12, 94);
+            this.outputTxt.Name = "outputTxt";
+            this.outputTxt.ReadOnly = true;
+            this.outputTxt.Size = new System.Drawing.Size(776, 20);
+            this.outputTxt.TabIndex = 5;
+            this.outputTxt.Text = "Output";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.ClientSize = new System.Drawing.Size(800, 120);
+            this.Controls.Add(this.outputTxt);
+            this.Controls.Add(this.decryptBtn);
+            this.Controls.Add(this.encryptBtn);
+            this.Controls.Add(this.inputSepearator);
+            this.Controls.Add(this.inputKey);
+            this.Controls.Add(this.inputTxt);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Prime Number Encryptor";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox inputTxt;
+        private System.Windows.Forms.TextBox inputKey;
+        private System.Windows.Forms.TextBox inputSepearator;
+        private System.Windows.Forms.Button encryptBtn;
+        private System.Windows.Forms.Button decryptBtn;
+        private System.Windows.Forms.TextBox outputTxt;
+    }
+}
