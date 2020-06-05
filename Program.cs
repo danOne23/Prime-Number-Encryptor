ce2206e5af3d03cf60da62210e4e41f3ddc988a8
danOne23 <danonemun@gmail.com>
2020-06-05 19:57:01 +0200

    Designed UI

--- Program.cs
+++ Program.cs
@@ -7,11 +7,19 @@ using System.Windows.Forms;
 
 namespace encryptor
 {
+
     class Program
     {
+        [STAThread]
+
         static void Main(string[] args)
         {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
         }
+
+        public static string Encrypt(string input, string key, string seperator) => new Encryptor(input, Int64.Parse(key), seperator).encryptedStr;
+        public static string Decrypt(string input, string key, string seperator) => new Decryptor(input, Int64.Parse(key), seperator).decryptedStr;
     }
 }
